c864cf8fdb6159aaf06e0dfe653768f81ff4fe61
Štěpán Štícha <stichastepan@gmail.com>
2025-11-14 16:11:53 +0100

    přidání fillable do modelů

--- app/Models/Pub.php
+++ app/Models/Pub.php
@@ -6,6 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pub extends Model
 {
+    protected $fillable = [
+        'name',
+        'description',
+        'latitude',
+        'longitude',
+        'street',
+        'city',
+        'postcode',
+        'image',
+        'website',
+        'phone',
+        'overpass_node_id',
+    ];
+
     public function reviews(){
         return $this->hasMany(Review::class);
     }
